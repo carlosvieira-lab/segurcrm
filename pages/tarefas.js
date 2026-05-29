79e503e17107b9f607947c0bcb8ae9ed349c6002
carlosvieira-lab <carlos.vieira@lojasegurostrajouce.com>
2026-05-29 11:13:09 +0100

    Update tarefas.js

--- pages/tarefas.js
+++ pages/tarefas.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+mport { useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { createClient } from "@supabase/supabase-js";
