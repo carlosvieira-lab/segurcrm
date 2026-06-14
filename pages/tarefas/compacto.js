ea63c3576d454e31e9fca3aa5653510db930a0d2
carlosvieira-lab <carlos.vieira@lojasegurostrajouce.com>
2026-06-14 22:31:03 +0100

    Update compacto.js

--- pages/tarefas/compacto.js
+++ pages/tarefas/compacto.js
@@ -1,4 +1,4 @@
-mport { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { createClient } from "@supabase/supabase-js";
